a38ab64da8b099f09bc52f52b153737aaafb1930
홍짜이 <121369228+h-lawliet@users.noreply.github.com>
2024-11-27 21:14:00 +0900

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,6 @@
 import schedule
 import time
-import subprocess
-from datetime import datetime
+import os
 
 script_name = "main.py"
 
@@ -9,11 +8,12 @@ def run_script():
     print(f"Running {script_name}")
     os.system(f"python3 {script_name}")
 
-# 매일 오전 5시에 실행
-schedule.every().day.at("21:10").do(run_script)
+# 매일 5시 스케줄 등록
+schedule.every().day.at("21:15").do(run_script)
 
 print("Scheduler started. Waiting for the time to execute...")
 
+# 무한 루프로 스케줄러 동작
 while True:
     schedule.run_pending()
     time.sleep(1)
