b2f91b01ebbe98190d7dc38bb214faff87809c29
leewish03 <lekas1217@yonsei.ac.kr>
2025-01-30 00:28:23 +0900

    Update scheduler.py
    시간 및 메세지 수정

--- scheduler.py
+++ scheduler.py
@@ -5,15 +5,15 @@ import os
 script_name = "main.py"
 
 def run_script():
-    print(f"Running {script_name}")
+    print(f"Running {script_name} at scheduled time...")
     os.system(f"python3 {script_name}")
 
 # 매일 5시 스케줄 등록
-schedule.every().day.at("22:26").do(run_script)
+schedule.every().day.at("05:00").do(run_script)
 
-print("Scheduler started. Waiting for the time to execute...")
+print("Scheduler started. Waiting for the scheduled time to execute...")
 
 # 무한 루프로 스케줄러 동작
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(60)
